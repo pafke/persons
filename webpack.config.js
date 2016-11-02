f5cd96ea5d07f2682668c915ad06e6123d5156df
Timo <timo@haragara.com>
2016-11-02 19:28:21 +0100

    BASE

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require("webpack");
+module.exports = {
+    entry: './src/base.jsx',
+    output: {
+        path: './build/js/',
+        publicPath: "/js/",
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: "./build",
+        inline: true,
+        port: 3333
+    },
+    module: {
+        /*loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]*/
+    },
+    plugins: [
+        //new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
+}
