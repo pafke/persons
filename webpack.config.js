9f5b57f842729847085e5aa0935f483ab072e343
Timo <timo@haragara.com>
2016-11-03 21:35:12 +0100

    NPM: Added module and command to minify CSS

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,6 @@ module.exports = {
         ]
     },
     plugins: [
-        //new webpack.optimize.UglifyJsPlugin({minimize: true})
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
     ]
 }
